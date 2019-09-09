d7b3bf49d2db76d1967ea772d70265bc4f37f798
Vladislav Burlutskiy <vladislavburlutskiy@MacBook-Pro.local>
2019-09-09 11:06:09 -0400

    installed react-router-dom dependency, created components folder

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,8 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { Route, Link } from 'react-router-dom';
+
 
 function App() {
+
+
   return (
     <div className="App">
 
